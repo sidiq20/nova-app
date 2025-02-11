21b87836432ac719079af6f288453e6bc4332944
sidiq20 <sidiqolasode@gmail.com>
2025-02-11 14:50:56 +0100

    initial commit

---
+++ src/data/stickers.ts
@@ -0,0 +1,8 @@
+export const stickers = [
+  { id: 'heart', emoji: '❤️' },
+  { id: 'rose', emoji: '🌹' },
+  { id: 'kiss', emoji: '💋' },
+  { id: 'ring', emoji: '💍' },
+  { id: 'dove', emoji: '🕊️' },
+  { id: 'cupid', emoji: '👼' }
+];
