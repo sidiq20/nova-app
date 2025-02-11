9e943e190c7c97998fe8be7674153502a2b7d66f
sidiq20 <sidiqolasode@gmail.com>
2025-02-11 14:50:29 +0100

    initial commit

---
+++ src/data/colors.ts
@@ -0,0 +1,8 @@
+export const colors = [
+  { id: 'white', name: 'White', value: '#ffffff', border: 'border-gray-200' },
+  { id: 'cream', name: 'Cream', value: '#fff8e7', border: 'border-yellow-200' },
+  { id: 'rose', name: 'Rose', value: '#ffe4e6', border: 'border-rose-200' },
+  { id: 'mint', name: 'Mint', value: '#dcfce7', border: 'border-green-200' },
+  { id: 'sky', name: 'Sky', value: '#e0f2fe', border: 'border-blue-200' },
+  { id: 'lavender', name: 'Lavender', value: '#ede9fe', border: 'border-purple-200' }
+];
