8f19d306f81b0846f06052b6ba8460ecd0960cc4
sidiq20 <sidiqolasode@gmail.com>
2025-02-12 23:27:44 +0100

    updated app

--- src/data/papers.ts
+++ src/data/papers.ts
@@ -1,22 +1,32 @@
 export const papers = {
   classic: {
-    name: 'Classic White',
+    name: '',
     preview: '/assets/paper/paper1.png',
     full: '/assets/paper/paper1.png'
   },
   parchment: {
-    name: 'Vintage Parchment',
-    preview: 'https://images.unsplash.com/photo-1597336374828-43e9de844c61?auto=format&fit=crop&q=80&w=400',
-    full: 'https://images.unsplash.com/photo-1597336374828-43e9de844c61?auto=format&fit=crop&q=80&w=2000'
+    name: '',
+    preview: '/assets/paper/paper2.jpg',
+    full: '/assets/paper/paper2.jpg'
   },
   cream: {
-    name: 'Cream Texture',
-    preview: 'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?auto=format&fit=crop&q=80&w=400',
-    full: 'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?auto=format&fit=crop&q=80&w=2000'
+    name: '',
+    preview: '/assets/paper/paper3.jpg',
+    full: '/assets/paper/paper3.jpg'
   },
   creampie: {
-    name: 'carton',
+    name: '',
     preview: '/assets/paper/paper6.jpg',
     full: '/assets/paper/paper6.jpg'
+  },
+    paper: {
+    name: '',
+    preview: '/assets/paper/paper4.jpg',
+    full: '/assets/paper/paper4.jpg'
+  },
+    exercise: {
+    name: '',
+    preview: '/assets/paper/paper5.jpg',
+    full: '/assets/paper/paper5.jpg'
   },
 };
