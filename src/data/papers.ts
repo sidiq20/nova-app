2508a5629792050664ff32fb4cb1d420968047e1
sidiq20 <sidiqolasode@gmail.com>
2025-02-12 13:55:46 +0100

    updated app

--- src/data/papers.ts
+++ src/data/papers.ts
@@ -1,8 +1,8 @@
 export const papers = {
   classic: {
     name: 'Classic White',
-    preview: 'https://images.unsplash.com/photo-1518893494013-481c1d8ed3fd?auto=format&fit=crop&q=80&w=400',
-    full: 'https://images.unsplash.com/photo-1518893494013-481c1d8ed3fd?auto=format&fit=crop&q=80&w=2000'
+    preview: '/assets/paper/paper1.png',
+    full: '/assets/paper/paper1.png'
   },
   parchment: {
     name: 'Vintage Parchment',
@@ -13,5 +13,10 @@ export const papers = {
     name: 'Cream Texture',
     preview: 'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?auto=format&fit=crop&q=80&w=400',
     full: 'https://images.unsplash.com/photo-1586075010923-2dd4570fb338?auto=format&fit=crop&q=80&w=2000'
-  }
+  },
+  creampie: {
+    name: 'carton',
+    preview: '/assets/paper/paper6.jpg',
+    full: '/assets/paper/paper6.jpg'
+  },
 };
