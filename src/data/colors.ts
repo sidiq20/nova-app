c893fd334b0addbff442c37bfb441fc879574a18
sidiq20 <sidiqolasode@gmail.com>
2025-02-12 21:22:40 +0100

    updated app

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -4,5 +4,11 @@ export const colors = [
   { id: 'rose', name: 'Rose', value: '#ffe4e6', border: 'border-rose-200' },
   { id: 'mint', name: 'Mint', value: '#dcfce7', border: 'border-green-200' },
   { id: 'sky', name: 'Sky', value: '#e0f2fe', border: 'border-blue-200' },
-  { id: 'lavender', name: 'Lavender', value: '#ede9fe', border: 'border-purple-200' }
+  { id: 'lavender', name: 'Lavender', value: '#ede9fe', border: 'border-purple-200' },
+  { id: 'peach', name: 'Peach', value: '#ffd7cc', border: 'border-orange-200' },
+  { id: 'sage', name: 'Sage', value: '#e2e8d5', border: 'border-green-200' },
+  { id: 'blush', name: 'Blush', value: '#fce7f3', border: 'border-pink-200' },
+  { id: 'seashell', name: 'Seashell', value: '#fff5ee', border: 'border-orange-100' },
+  { id: 'honeydew', name: 'Honeydew', value: '#f0fff0', border: 'border-green-100' },
+  { id: 'azure', name: 'Azure', value: '#f0ffff', border: 'border-cyan-100' }
 ];
